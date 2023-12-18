65949c15fdcea10868a0217c3650dd9a273a43f7
pertinax1 <david@yourcompanycio.com>
2023-12-17 17:13:21 -0700

    Program working!

--- customer_banking.py
+++ customer_banking.py
@@ -1,5 +1,7 @@
 # Import the create_cd_account and create_savings_account functions
 # ADD YOUR CODE HERE
+from savings_account import create_savings_account
+from cd_account import create_cd_account
 
 # Define the main function
 def main():
@@ -9,21 +11,32 @@ def main():
     """
     # Prompt the user to set the savings balance, interest rate, and months for the savings account.
     # ADD YOUR CODE HERE
+    savings_balance = float(input('Enter your savings starting balance: '))
+    savings_interest = float(input('Enter your interest rate: '))
+    savings_maturity = float(input('Enter the number of months that interest will be earned: '))
 
     # Call the create_savings_account function and pass the variables from the user.
     updated_savings_balance, interest_earned = create_savings_account(savings_balance, savings_interest, savings_maturity)
 
     # Print out the interest earned and updated savings account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
+    print(f'Interest earned is: {interest_earned}')
+    print(f'Updated balance is: {updated_savings_balance}')
 
     # Prompt the user to set the CD balance, interest rate, and months for the CD account.
     # ADD YOUR CODE HERE
+    cd_balance = float(input('Enter your CD starting balance: '))
+    cd_interest = float(input('Enter your annual percentiage interest rate/yield: '))
+    cd_maturity = float(input('Enter the duration of the CD term in months that interest will be earned: '))
 
     # Call the create_cd_account function and pass the variables from the user.
     updated_cd_balance, interest_earned = create_cd_account(cd_balance, cd_interest, cd_maturity)
 
     # Print out the interest earned and updated CD account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
+    print(f'Interest earned is: {interest_earned}')
+    print(f'Updated balance is: {updated_cd_balance}')
 
 if __name__ == "__main__":
     # Call the main function.
+    main()
